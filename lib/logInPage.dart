e777b4c7df6a3180ff145886b50d37715eb68f2a
bharath12321 <102640553+bharath12321@users.noreply.github.com>
2022-10-06 23:02:37 +1300

    Log in page basic ui complete

--- lib/logInPage.dart
+++ lib/logInPage.dart
@@ -1,1 +1,120 @@
 import 'package:flutter/material.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({Key? key}) : super(key: key);
+
+  @override
+  State<LogInPage> createState() => LogInPageState();
+}
+
+class LogInPageState extends State<LogInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+            //Welcome Text
+            child: Center(
+                child: Column(children: [
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Welcome!',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26.0,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 1,
+                  ),
+                  const Text(
+                    'Please Log In',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18.0,
+                        color: Colors.white70),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //Username/Email
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(12.0)),
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Email',
+                            ),
+                          ),
+                        )),
+                  ),
+                  const SizedBox(height: 10),
+
+                  //Password
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(12.0)),
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Password',
+                            ),
+                          ),
+                        )),
+                  ),
+                  const SizedBox(height: 15),
+
+                  //Login
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 90),
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlueAccent,
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+
+                  // Not a member? Register Now!
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Not registered? ',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        'Register now!',
+                        style: TextStyle(color: Colors.cyan),
+                      )
+                    ],
+                  )
+                ]))));
+          }
+        }
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'logInPage.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(const Home());
 }
 
 class Home extends StatelessWidget {
-  //const test({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          'Mobile Wallet',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-              child: Image.asset('app_icons/app-icon.png'),
-            flex: 3,
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('1'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.red,
-              child: Text('2'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.yellow,
-              child: Text('3'),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text('Click'),
-        backgroundColor: Colors.purple[800],
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LogInPage(),
     );
   }
 }
