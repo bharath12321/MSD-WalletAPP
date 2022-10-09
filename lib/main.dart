0c34360944a6d90897cbe1a2333d9c3d9009efe3
bharath12321 <102640553+bharath12321@users.noreply.github.com>
2022-10-09 22:46:46 +1300

    "app/build.gradle showing error but app is installing fine and works now"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-
 import 'logInPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const Home());
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class Home extends StatelessWidget {
